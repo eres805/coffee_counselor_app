8b245c24e272ff7f4db4e1e24fa6321f6f59e5d0
eres805 <eres805@gmail.com>
2014-09-03 01:00:37 -0400

    deleted tasting notes and countries controller.  updated roasters and coffee beans

--- app/controllers/coffee_beans_controller.rb
+++ app/controllers/coffee_beans_controller.rb
@@ -1,3 +1,12 @@
 class CoffeeBeansController < ApplicationController
 
+def index
+	@coffee_beans = CoffeeBeans.all
+end
+
+def show
+	@coffee_bean = CoffeeBean.find(params[:id])
+	@bean_roaster = @coffee_bean.roaster.where(params[:roaster_id])
+end
+
 end
--- app/controllers/roasters_controller.rb
+++ app/controllers/roasters_controller.rb
@@ -1,3 +1,20 @@
 class RoastersController < ApplicationController
 
+	before_filter :determine_scope, :only => :index
+
+	def index
+		@roasters = Roaster.all
+	end
+
+	def show
+		@roaster = Roaster.find(params[:id])
+		@roaster_beans = @roaster.coffee_beans.where(params[:roaster_id])
+	end
+
+	private
+
+	def current_roaster
+		@current_roaster = Roaster.find(params[:roaster_id])
+	end
+
 end
