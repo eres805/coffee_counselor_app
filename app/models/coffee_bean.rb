3a62f16e73a16dabd4e92db440a8c20ef1d285b7
eres805 <eres805@gmail.com>
2014-11-21 07:11:54 -0500

    coffee_bean and user model updated

--- app/models/coffee_bean.rb
+++ app/models/coffee_bean.rb
@@ -3,7 +3,7 @@ class CoffeeBean < ActiveRecord::Base
 	belongs_to :roaster
 	belongs_to :country
 	has_many :flavors
-	has_many :users
+	belongs_to :user
 
 	def number_to_currency(number)
     	number_to_currency(number, :unit => "R$ ", :separator => ",", :delimiter => ".")
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,11 @@ class User < ActiveRecord::Base
 
 	has_secure_password
 	validates :first_name, :last_name, :email, :password, :password_confirmation, presence: true, :on => :create
-  validates :password, length: { minimum: 5 }, :on => :create
+	validates :password, length: { minimum: 5 }, :on => :create
+
+	has_many :coffee_beans
+	has_many :favorites
+	has_many :favorite_coffees, through: :favorites, source: :favorited, source_type: 'CoffeeBean'
 
 
 end
